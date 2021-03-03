7e67b538e4c1be71a6168dff73378457ee80637f
ctpower <332386415@qq.com>
2021-03-03 11:54:17 +0800

    bus 可以使用，但是配置文件还没有整合到一个文件里

--- springcloud-config-client-dev-3355/src/main/java/com/ctpower/springcloud/controller/ConfigClientController.java
+++ springcloud-config-client-dev-3355/src/main/java/com/ctpower/springcloud/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.ctpower.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Copyright www.ctpower.com
  */
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
     @Value("${spring.application.name}")
@@ -28,7 +30,8 @@ public class ConfigClientController {
     @ResponseBody
     public String getConfig(){
 
-        return "applicationName:" + applicationNanme +
+        return
+                "applicationName:" + applicationNanme +
                 "eurekaServer:"+eurekaServer +
                 "port:"+ port +
                 "hello:" + hello;
